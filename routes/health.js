e63d60d9fc92cc7efcf2ad570bff518c9d4e8cc7
eliserving <164472594+eliserving@users.noreply.github.com>
2026-03-23 02:10:38 -0600

    Create health.js

---
+++ routes/health.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json({ status: 'OK', uptime: process.uptime() })
+})
+
+module.exports = router
